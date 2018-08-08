4428ae9e80a637d9fd285fd12b73517e5472fc68
Kyle Cui <kyle.tz.cui@gmail.com>
2018-08-08 10:44:30 -0400

    Testing branch changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Text countText;
     public Text winText;
+    public string gitTestString;
 
     private Rigidbody rb;
     private int count;
